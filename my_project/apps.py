f2054cc2b5562fba2a42802beae4f0c848053fa3
Linus Johansson <golfarenlj@gmail.com>
2024-04-25 15:32:21 +0000

    Changed back to Djangos default 'Single quotes' after formatting

--- my_project/apps.py
+++ my_project/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MyProjectConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "my_project"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_project'
--- tech_corner/urls.py
+++ tech_corner/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 from my_project import views as index_views
 
 urlpatterns = [
-    path("", index_views.index, name="index"),
-    path("admin/", admin.site.urls),
+    path('', index_views.index, name='index'),
+    path('admin/', admin.site.urls),
 ]
