d8e565a89d6ca7f17444e7cd269b1aeab21e8d8b
Linus Johansson <golfarenlj@gmail.com>
2024-05-04 21:33:18 +0000

    Initial deployment commit

--- tech_corner/settings.py
+++ tech_corner/settings.py
@@ -92,6 +92,11 @@ WSGI_APPLICATION = 'tech_corner.wsgi.application'
 DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com/"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
