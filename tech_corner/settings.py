ce6d2cc12287e7a93347303f6dcfe73bc7c5466c
Linus Johansson <golfarenlj@gmail.com>
2024-05-04 19:54:34 +0000

    edit database

--- tech_corner/settings.py
+++ tech_corner/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'tech_corner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
