bcf381f208e32ff9c94c6d238e2d42f6476d10d6
Linus Johansson <golfarenlj@gmail.com>
2024-05-04 18:36:36 +0000

    added blog to installed apps

--- tech_corner/settings.py
+++ tech_corner/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my_project',
+    'website',
+    'blog',
 ]
 
 MIDDLEWARE = [
